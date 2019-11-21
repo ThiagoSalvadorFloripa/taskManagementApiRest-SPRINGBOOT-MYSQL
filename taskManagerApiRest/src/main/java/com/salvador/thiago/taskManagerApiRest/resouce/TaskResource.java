c221298e385758bdbb09cac7692a33bd609691a3
thiafo <thia>
2019-11-20 23:15:48 -0300

    Criação metodo findPage

--- taskManagerApiRest/src/main/java/com/salvador/thiago/taskManagerApiRest/resouce/TaskResource.java
+++ taskManagerApiRest/src/main/java/com/salvador/thiago/taskManagerApiRest/resouce/TaskResource.java
@@ -6,10 +6,12 @@ package com.salvador.thiago.taskManagerApiRest.resouce;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.salvador.thiago.taskManagerApiRest.domain.Task;
@@ -37,6 +39,17 @@ public class TaskResource {
 		List<Task> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@RequestMapping(value = "/page",method = RequestMethod.GET)
+	public ResponseEntity<Page<Task>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "current") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+		Page<Task> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
+	}
+	
 	
 	
 
--- taskManagerApiRest/src/main/java/com/salvador/thiago/taskManagerApiRest/service/TaskService.java
+++ taskManagerApiRest/src/main/java/com/salvador/thiago/taskManagerApiRest/service/TaskService.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.salvador.thiago.taskManagerApiRest.domain.Task;
@@ -22,6 +25,8 @@ public class TaskService {
 	
 	@Autowired
 	private TaskRepository repository;
+	
+	
 
 	public Task findById(Long id) {
 		Optional<Task> obj = repository.findById(id);
@@ -31,6 +36,11 @@ public class TaskService {
 
 	public List<Task> findAll() {
 		return repository.findAll();
+	}
+
+	public Page<Task> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest =PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll(pageRequest);
 	}
 
 }
